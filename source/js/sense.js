362ad7aeafc435db82f0e98e55b00c14817e55fd
M-finder <m@m-finder.com>
2019-08-24 22:18:41 +0800

    update

--- source/js/sense.js
+++ source/js/sense.js
@@ -9,7 +9,6 @@ $('.nav-toggle-btn').click(() => {
 });
 
 $('#go-top').click(() => {
-  console.log('0000');
   $('body,html').animate({ scrollTop: 0 }, 700);
 });
 function scrollBar() {
@@ -19,21 +18,20 @@ function scrollBar() {
     let o = $(window).height();
     let a = parseInt(e / (t - o) * 100);
     $('#bar').css('width', `${a}%`);
-    $('.toc-container').css('height', $('.site-content').outerHeight());
-    $('.skin-menu').removeClass('show');
   });
 }
 function goTop() {
   let e = $('#go-top');
   $(window).scroll(function () {
-    $(this).scrollTop() > 200 ? (e.addClass('cd-is-visible'),
-    $('.changeSkin-gear').css('bottom', '0'),
-    $(window).height() > 950
-      ? $('.cd-top.cd-is-visible').css('top', '0')
-      : $('.cd-top.cd-is-visible').css('top', `${$(window).height() - 950}px`))
-      : ($('.changeSkin-gear').css('bottom', '-999px'),
-      $('.cd-top.cd-is-visible').css('top', '-900px'),
-      e.removeClass('cd-is-visible cd-fade-out')),
+    $(this).scrollTop() > 200
+      ? (e.addClass('cd-is-visible'),
+          $('.header').css({ 'box-shadow': '0 1px 40px -8px rgba(0, 0, 0, .5)' }),
+          $(window).height() > 950
+            ? $('.cd-top.cd-is-visible').css('top', '0')
+            : $('.cd-top.cd-is-visible').css('top', `${$(window).height() - 950}px`))
+      : ($('.cd-top.cd-is-visible').css('top', '-900px'),
+        e.removeClass('cd-is-visible cd-fade-out'),
+        $('.header').css({ 'box-shadow': 'none' }));
     $(this).scrollTop() > 1200 && e.addClass('cd-fade-out');
   });
 }
