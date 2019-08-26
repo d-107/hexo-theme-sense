6344be812de63994f6b917894b320ac56977cf8a
wuyunfang <wuyunfang@minzhetech.com>
2019-08-26 17:41:32 +0800

    update

--- source/js/sense.js
+++ source/js/sense.js
@@ -1,16 +1,17 @@
 $('.nav-toggle-btn').click(() => {
-  if ($('.site-nav').hasClass('hide-nav')) {
-    $('.nav-toggle-btn').children('i').html('&#xe77c;');
-    $('.site-nav').removeClass('hide-nav');
-  } else {
+  if ($('.site-nav').hasClass('show-nav')) {
     $('.nav-toggle-btn').children('i').html('&#xe600;');
-    $('.site-nav').addClass('hide-nav');
+    $('.site-nav').removeClass('show-nav');
+  } else {
+    $('.nav-toggle-btn').children('i').html('&#xe77c;');
+    $('.site-nav').addClass('show-nav');
   }
 });
 
 $('#go-top').click(() => {
   $('body,html').animate({ scrollTop: 0 }, 700);
 });
+
 function scrollBar() {
   document.body.clientWidth > 860 && $(window).scroll(() => {
     let e = $(window).scrollTop();
@@ -24,15 +25,15 @@ function goTop() {
   let e = $('#go-top');
   $(window).scroll(function () {
     $(this).scrollTop() > 200
-      ? (e.addClass('cd-is-visible'),
+      ? (e.addClass('go-top-is-visible'),
           $('.header').css({ 'box-shadow': '0 1px 40px -8px rgba(0, 0, 0, .5)' }),
           $(window).height() > 950
-            ? $('.cd-top.cd-is-visible').css('top', '0')
-            : $('.cd-top.cd-is-visible').css('top', `${$(window).height() - 950}px`))
-      : ($('.cd-top.cd-is-visible').css('top', '-900px'),
-        e.removeClass('cd-is-visible cd-fade-out'),
+            ? $('.go-top.go-top-is-visible').css('top', '0')
+            : $('.go-top.go-top-is-visible').css('top', `${$(window).height() - 950}px`))
+      : ($('.go-top.go-top-is-visible').css('top', '-900px'),
+        e.removeClass('go-top-is-visible go-top-fade-out'),
         $('.header').css({ 'box-shadow': 'none' }));
-    $(this).scrollTop() > 1200 && e.addClass('cd-fade-out');
+    $(this).scrollTop() > 1200 && e.addClass('go-top-fade-out');
   });
 }
 
