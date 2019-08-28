37a66f84c6c5441bdd91009dfec5e801914ba909
wuyunfang <wuyunfang@minzhetech.com>
2019-08-28 14:24:00 +0800

    update

--- source/js/sense.js
+++ source/js/sense.js
@@ -1,7 +1,5 @@
-
-$(function () {
-
-  let articles = new Array();
+$(() => {
+  let articles = [];
 
   $('.nav-toggle-btn').click((event) => {
     if ($('.site-nav').hasClass('show-nav')) {
@@ -11,27 +9,27 @@ $(function () {
       $('.nav-toggle-btn').children('i').html('&#xe77c;');
       $('.site-nav').addClass('show-nav');
     }
-      event.stopPropagation()
+    event.stopPropagation();
   });
 
   $('.site-nav').click((event) => {
-      event.stopPropagation()
+    event.stopPropagation();
   });
 
-  $(document).on('click',function(){
+  $(document).on('click', () => {
     hideMenu();
   });
 
-  //滚动事件
+  // 滚动事件
   // $(document).on('mousewheel',function(){
   //   hideMenu();
   // });
-  //滑动事件
+  // 滑动事件
   // $(document).on('touchmove',function(){
   //   hideMenu();
   // });
 
-  function hideMenu(){
+  function hideMenu() {
     if ($('.site-nav').hasClass('show-nav')) {
       $('.nav-toggle-btn').children('i').html('&#xe600;');
       $('.site-nav').removeClass('show-nav');
@@ -41,24 +39,23 @@ $(function () {
     }
   }
 
-  $('.search-toggle-btn').click((event)=>{
-    console.log($('#search').hasClass('show-search'))
-      if ($('#search').hasClass('show-search')) {
-          $('#search').removeClass('show-search');
-      } else {
-          $('#search').addClass('show-search');
-      }
-      event.stopPropagation()
+  $('.search-toggle-btn').click((event) => {
+    console.log($('#search').hasClass('show-search'));
+    if ($('#search').hasClass('show-search')) {
+      $('#search').removeClass('show-search');
+    } else {
+      $('#search').addClass('show-search');
+    }
+    event.stopPropagation();
   });
 
-   $('#search').click((event) => {
-      event.stopPropagation()
-   });
+  $('#search').click((event) => {
+    event.stopPropagation();
+  });
 
   $('#go-top').click(() => {
     $('body,html').animate({ scrollTop: 0 }, 700);
   });
-
 
 
   function search() {
@@ -69,40 +66,38 @@ $(function () {
 
   function find(value) {
     const lists = $('.search-article');
-    //每次查询前,隐藏
-    lists.each(function(){
+    // 每次查询前,隐藏
+    lists.each(function () {
       $(this).addClass('hide');
     });
-    let arr = new Array();
-    if(value.trim().length<=0)return arr;
+    const arr = new Array();
+    if (value.trim().length <= 0) return arr;
     let isTag = false;
     let index = -1;
 
-    if(value.indexOf('#') == 0) {
-      isTag =true;
-      value = value.replace('#','');
+    if (value.indexOf('#') == 0) {
+      isTag = true;
+      value = value.replace('#', '');
     }
-    for(let i=0;i<articles.length;i++) {
-      let article = articles[i];
-      console.log(article)
+    for (let i = 0; i < articles.length; i++) {
+      const article = articles[i];
+      console.log(article);
       index = -1;
-      if(isTag) {
-        if(article.tags.indexOf(value) >=0){
+      if (isTag) {
+        if (article.tags.indexOf(value) >= 0) {
           index = article.index;
         }
-      }else {
-        if(article.name.indexOf(value) >=0) {
-          index = article.index;
-        }
+      } else if (article.name.indexOf(value) >= 0) {
+        index = article.index;
       }
-      if(index>0) {
-        //console.log(article);
+      if (index > 0) {
+        // console.log(article);
         arr.push(index);
       }
     }
-    if(arr.length>0) {
-      for(let i=0;i<lists.length;i++) {
-        if(arr.indexOf(i) >=0) {
+    if (arr.length > 0) {
+      for (let i = 0; i < lists.length; i++) {
+        if (arr.indexOf(i) >= 0) {
           $(lists[i]).removeClass('hide');
         }
       }
@@ -111,7 +106,7 @@ $(function () {
 
   function initArticles() {
     const lists = $('#search-articles').children();
-    //console.log(lists);
+    // console.log(lists);
     let index = 0;
     lists.each(function () {
       const name = $(this).attr('data-name');
@@ -124,55 +119,55 @@ $(function () {
           tags.push(tag);
         }
       });
-      const model = { name: name, url: url, date: date, tags: tags, index: index };
+      const model = {
+        name, url, date, tags, index,
+      };
       articles.push(model);
       index++;
     });
-    //console.log(articles);
+    // console.log(articles);
   }
 
-  $('#txtsearch').on('input', function () {
+  $('#txtsearch').on('input', () => {
     search();
   });
 
-  $('#search-tags').children().each(function(){
-    $(this).on('click',function(){
+  $('#search-tags').children().each(function () {
+    $(this).on('click', function () {
       $('#txtsearch').val('');
-      const value = '#'+$(this).html().trim();
+      const value = `#${$(this).html().trim()}`;
       $('#txtsearch').val(value);
       find(value);
     });
   });
 
-    function scrollBar() {
-        document.body.clientWidth > 860 && $(window).scroll(() => {
-            let e = $(window).scrollTop();
-            let t = $(document).height();
-            let o = $(window).height();
-            let a = parseInt(e / (t - o) * 100);
-            $('#bar').css('width', `${a}%`);
-        });
-    }
+  function scrollBar() {
+    document.body.clientWidth > 860 && $(window).scroll(() => {
+      const e = $(window).scrollTop();
+      const t = $(document).height();
+      const o = $(window).height();
+      const a = parseInt(e / (t - o) * 100);
+      $('#bar').css('width', `${a}%`);
+    });
+  }
 
-    function goTop() {
-        let e = $('#go-top');
-        $(window).scroll(function () {
-            $(this).scrollTop() > 200
-                ? (e.addClass('go-top-is-visible'),
-                    $('.header').css({ 'box-shadow': '0 1px 40px -8px rgba(0, 0, 0, .5)' }),
-                    $(window).height() > 950
-                        ? $('.go-top.go-top-is-visible').css('top', '0')
-                        : $('.go-top.go-top-is-visible').css('top', `${$(window).height() - 950}px`))
-                : ($('.go-top.go-top-is-visible').css('top', '-900px'),
-                    e.removeClass('go-top-is-visible go-top-fade-out'),
-                    $('.header').css({ 'box-shadow': 'none' }));
-            $(this).scrollTop() > 1200 && e.addClass('go-top-fade-out');
-        });
-    }
+  function goTop() {
+    const e = $('#go-top');
+    $(window).scroll(function () {
+      $(this).scrollTop() > 200
+        ? (e.addClass('go-top-is-visible'),
+        $('.header').css({ 'box-shadow': '0 1px 40px -8px rgba(0, 0, 0, .5)' }),
+        $(window).height() > 950
+          ? $('.go-top.go-top-is-visible').css('top', '0')
+          : $('.go-top.go-top-is-visible').css('top', `${$(window).height() - 950}px`))
+        : ($('.go-top.go-top-is-visible').css('top', '-900px'),
+        e.removeClass('go-top-is-visible go-top-fade-out'),
+        $('.header').css({ 'box-shadow': 'none' }));
+      $(this).scrollTop() > 1200 && e.addClass('go-top-fade-out');
+    });
+  }
 
   scrollBar();
   goTop();
-
   initArticles();
 });
-
